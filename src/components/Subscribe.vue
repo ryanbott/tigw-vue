b27747b94f812b5665a8e7c96dbf367920e03ac0
Ryan Bott <rbott13@gmail.com>
2018-12-24 16:19:49 -0600

    favicon and subscribe buttons

--- src/components/Subscribe.vue
+++ src/components/Subscribe.vue
@@ -7,12 +7,12 @@
       <li class="subscribe__link subscribe__link--overcast">
         <a target="_blank" href="https://overcast.fm/itunes1435155571/things-i-got-wrong">Overcast</a>
       </li>
-      <li class="subscribe__link subscribe__link--google">
-        <a target="_blank" href="https://www.google.com/podcasts?feed=aHR0cHM6Ly9hbmNob3IuZm0vcy82OThlZTA0L3BvZGNhc3QvcnNz">Google Podcast</a>
-      </li>
       <li class="subscribe__link subscribe__link--stitcher">
         <a target="_blank" href="https://www.stitcher.com/podcast/anchor-podcasts/things-i-got-wrong">Stitcher
         </a>
+      </li>
+      <li class="subscribe__link subscribe__link--google">
+        <a target="_blank" href="https://www.google.com/podcasts?feed=aHR0cHM6Ly9hbmNob3IuZm0vcy82OThlZTA0L3BvZGNhc3QvcnNz">Google Podcast</a>
       </li>
       <li class="subscribe__link subscribe__link--pocketcasts">
         <a target="_blank" href="https://pca.st/cx6C">PocketCasts</a>
